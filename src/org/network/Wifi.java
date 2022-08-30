601a17717f13a70d48b7b3169dbd3a5288931732
pareekrajni77 <rajniparks@gmail.com>
2022-08-30 18:00:40 +0530

    Lines of code added

---
+++ src/org/network/Wifi.java
@@ -0,0 +1,31 @@
+package org.network;
+
+public class Wifi {
+	
+public void wifiName() {
+System.out.println("Wifi Name is Jiofiber");
+	}
+
+	public static void main(String[] args) {
+		
+		Wifi w = new Wifi();
+		w.wifiName();
+		
+		MobileData md = new MobileData();
+		md.dataName();
+		
+		Lan l = new Lan();
+		l.lanName();
+		
+		Wireless wl = new Wireless();
+		wl.modamName();
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ src/org/network/Wireless.java
@@ -0,0 +1,9 @@
+package org.network;
+
+public class Wireless {
+	
+public void modamName() {
+System.out.println("Modam Name is R ");
+	}
+
+}
